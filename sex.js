120a79ae767e3855369f4c90ac40cbfcc17ba340
Osama Bin Laden <38994707+ogbinladen@users.noreply.github.com>
2019-01-24 16:31:23 -0500

    Add files via upload

--- sex.js
+++ sex.js
@@ -7,3 +7,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
   });
+
+var leftChart = new Chart("leftChart",{
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data:[120,60,175,90],
+            "backgroundColor":["rgb(49, 232, 194)","rgb(255, 230, 61)","rgb(232, 55, 64)","rgb(185, 48, 255)"]
+        }],
+        labels: [
+            'Founder Capital',
+            'Private Funding',
+            'Investments',
+            'Capital Gain'
+        ]
+    },
+    options: {}
+});
+
+var rightChart = new Chart("rightChart",{
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data:[265,20,40,120],
+            "backgroundColor":["rgb(49, 232, 194)","rgb(255, 230, 61)","rgb(232, 55, 64)","rgb(185, 48, 255)"]
+        }],
+        labels: [
+            'Community Giving',
+            'R&D',
+            'Equipment',
+            'Savings/Reinvestment'
+        ]
+    },
+    options: {rotation:0.5*Math.PI}
+});
